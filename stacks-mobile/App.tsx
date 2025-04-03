bea3a8d1de9a2d841e430aecd20553f873255dc8
Subhajit Kundu <subha60kundu@gmail.com>
2025-04-03 13:14:45 +0530

    core: configure nativewind for styling

---
+++ stacks-mobile/App.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-slate-800 text-2xl font-bold">
+        Welcome to Stacks Mobile
+      </Text>
+      <Text className="text-slate-600 mt-2">
+        Start editing by modifying app/index.tsx
+      </Text>
+    </View>
+  );
+}
+
