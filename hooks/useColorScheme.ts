72c617878f7c4a33c951a66e37dced3c917bb9d5
Subhajit Kundu <subha60kundu@gmail.com>
2025-07-25 10:30:11 +0530

    fix: dark mode not being persistent

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -1,6 +1,6 @@
-import { useColorScheme as _useColorScheme } from 'react-native';
+import {Appearance} from 'react-native';
 
 export function useColorScheme(): 'light' | 'dark' {
   // Always return 'light' to force light mode throughout the app
-  return 'light';
+  return Appearance.getColorScheme() ?? 'light'
 }
